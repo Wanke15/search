cf945b552d85421defb908cfabfffac867e3c344
wangke <1015005300@qq.com>
2022-10-13 19:53:29 +0800

    算法管理平台界面简单设计

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,45 @@
 <template>
     <el-container class="home-container">
-        <el-header>机器学习算法平台</el-header>
+        <el-header>搜索域算法管理平台</el-header>
+
         <el-container>
-            <el-aside width="200px">菜单栏</el-aside>
-            <el-main>内容</el-main>
+            <el-aside width="200px">
+              <el-menu :default-openeds="['1', '2']">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-search"></i>搜索&推荐</template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">召回</el-menu-item>
+          <el-menu-item index="1-2">排序</el-menu-item>
+          <el-menu-item index="1-3">NLP</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title"><i class="el-icon-food"></i>最佳搭配</template>
+        <el-menu-item-group>
+          <template slot="title">策略</template>
+          <el-menu-item index="2-1">财务类别打散</el-menu-item>
+          <el-menu-item index="2-2">首页业务规则</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="2-3">
+          <template slot="title"><i class="el-icon-cpu"></i>排序模型</template>
+          <el-menu-item index="2-3-1">DeepFM</el-menu-item>
+          <el-menu-item index="2-3-2">MMoE</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+    </el-menu>
+            </el-aside>
+
+            <el-main>
+              
+            </el-main>
+
         </el-container>
     </el-container>
 </template>
 
 <script>
+import "element-ui/lib/theme-chalk/index.css"
+
 export default {
     
 }
@@ -23,6 +54,8 @@ export default {
     background-color: #B3C0D1;
     color: #333;
     text-align: left;
+    font-size: large;
+    font-weight: bold;
     line-height: 60px;
   }
   
@@ -38,6 +71,10 @@ export default {
     color: #333;
     text-align: center;
     line-height: 160px;
+  }
+
+  .first-level-menu {
+    font-weight: bold;
   }
 
 </style>
